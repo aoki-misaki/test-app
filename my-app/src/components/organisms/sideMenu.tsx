06c09d384330152aee67804d714213f0f561b1c3
aoki-misaki <fan.happy.peaceful@gmail.com>
2025-02-07 23:02:55 +0900

    Update sideMenu.tsx

--- my-app/src/components/organisms/sideMenu.tsx
+++ my-app/src/components/organisms/sideMenu.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PersonIcon from '@mui/icons-material/Person';
+import * as styles from 'style'
 
 type Props = {};
 
@@ -7,7 +8,7 @@ const SideMenu = (props: Props) => {
   return (
     <nav className="fixed h-[100%] w-[230px] bg-mainColor">
     <a href="/">
-    <div className="sideMemuText">
+    <div className= {styles.sideMemuText}>
      <div>TestApp</div>    
      <div> <PersonIcon/> 社員情報管理</div>
       </div>
