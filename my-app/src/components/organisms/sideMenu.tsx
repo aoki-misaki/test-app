6db17c05f00e4fa0bd65cc7b93b3dc39549a341e
aoki-misaki <fan.happy.peaceful@gmail.com>
2025-02-07 15:44:18 +0900

    Update sideMenu.tsx

--- my-app/src/components/organisms/sideMenu.tsx
+++ my-app/src/components/organisms/sideMenu.tsx
@@ -5,7 +5,10 @@ type Props = {};
 
 const SideMenu = (props: Props) => {
   return (
+    <a href="/">
      <Personlcon/>
+      <div>社員情報<div/>
+    </a>
     <nav className="fixed h-[100%] w-[230px] bg-mainColor">サイドメニュー</nav>
   );
 };
