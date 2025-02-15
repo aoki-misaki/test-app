e26881aea479378068b90a727c4b452fe966ebfb
aoki-misaki <fan.happy.peaceful@gmail.com>
2025-02-15 21:22:26 +0900

    Update userList.tsx

--- my-app/src/components/pages/userList.tsx
+++ my-app/src/components/pages/userList.tsx
@@ -3,13 +3,14 @@ import React from "react";
 type Props = {};
 
 const UserList = (props: Props) => {
-  return
+  return (
   <div>
-   <>社員情報リスト</>;
-   <>全{
-    staffList.langth
-      }件</>
+   <div>社員情報リスト</div>
+   <div>全{
+    staffList.length
+      }件</div>
    </div>
+    )
 };
 
 export default UserList;
