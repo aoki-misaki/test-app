51787a0732f4056c39f3805346cf21c7ed630357
aoki-misaki <fan.happy.peaceful@gmail.com>
2025-02-21 15:53:48 +0900

    Update userList.tsx

--- my-app/src/components/pages/userList.tsx
+++ my-app/src/components/pages/userList.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { fetchUsersData } from "@/features/users/fetchUesrsData";
 import { useAppDispatch, useAppSelector } from "@/features/store";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 // 社員情報一覧画面
 const UserList = () => {
@@ -26,7 +27,7 @@ const UserList = () => {
     ｛
       users.map((user)=>{
         return(
-      <div></div>
+      <div><AccountCircleIcon/></div>
       <div>id</div>
       <div>name</div>
       })
