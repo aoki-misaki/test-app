40a2381c3d6e255307c0f3c712497fd6bccf1e43
aoki-misaki <fan.happy.peaceful@gmail.com>
2025-02-21 14:08:05 +0900

    Update userList.tsx

--- my-app/src/components/pages/userList.tsx
+++ my-app/src/components/pages/userList.tsx
@@ -16,20 +16,31 @@ const UserList = () => {
   // TODO メインコンテンツ部分を実装したら消す
   console.log(users);
 
- function App() {
- const userList =[
+[
    {
     id:00001,
     name:"山田　太郎",
-   }
+   },
+  {
+    id:00002,
+    name:"佐藤　鮎美"
+  }
    ]
   
-  return <>
+  return (
+    <>
     <>利用者情報</>
     <button class="inline-flex h-12 items-center justify-center rounded-md bg-sky-500 px-6 font-medium text-neutral-50 transition active:scale-110 ">新規登録</button>
     <>利用者情報リスト</>
     <>全{userList.length}件</>
+    ｛
+      userList.map(()=>{
+        return(
+        <Hello user={user}/>
+        )
+      })
+      ｝
   </>;
+  );
 };
-}
 export default UserList;
